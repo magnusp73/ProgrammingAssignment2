a51897755172c0e8784f180a26a48e0dd1d64951
magnusp73 <magnusp@openmailbox.org>
2016-01-24 22:30:55 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,41 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function reads a matrix and cashes the result. 
+## The cashed matrix can be retrieved by other functions. 
 
+makeCacheMatrix <- function(x = matrix()) {
+  I <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    I <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) I <<- inverse
+  getinverse <- function() I
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
+## This function reads a cashed matrix. If the inverse is already 
+## calculated, the cashed result is returned, else the inverse is
+## written to the cashed list variable.
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  I <- x$getinverse()
+  if(!is.null(I)) {
+    message("getting cached data")
+    return(I)
+  }
+  data <- x$get()
+  I <- solve(data, ...)
+  x$setinverse(I)
+  I
+
 }
